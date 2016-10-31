2306bcdfade698a5ea8b8888dab15b8f42f7c3dd
kpskidd <kapish.psingh.civ12@itbhu.ac.in>
2016-11-01 03:35:28 +0530

    updated oauth secrets,base.html and sentiment processing

--- sentimeter/sentimeter.py
+++ sentimeter/sentimeter.py
@@ -1,12 +1,34 @@
 __author__ = 'kapish'
 from secrets import Oauth_Secrets
 import tweepy
-from tweepy import OAuthHandler
+from textblob import TextBlob
 
 def primary(input_hashtag):
-    s_auth(input_hashtag)
-
-def s_auth(input_hashtag):
-    secrets = Oauth_Secrets()
-    auth = OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+    secrets = Oauth_Secrets()       #secrets imported from secrets.py
+    auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
     auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+
+    api = tweepy.API(auth)
+
+    N = 100                         #Number of Tweets
+    Tweets = tweepy.Cursor(api.search, q=input_hashtag).items(N)
+    neg=0.0
+    n=0.0
+    net=0.0
+    pos=0.0
+    p=0.0
+    for tweet in Tweets:
+        print tweet.text
+        blob = TextBlob(tweet.text)
+        if blob.sentiment.polarity < 0:         #Negative
+            neg+=blob.sentiment.polarity
+            n+=1
+        elif blob.sentiment.polarity == 0:      #Neutral
+            net+=1
+        else:                                   #Positive
+            pos+=blob.sentiment.polarity
+            p+=1
+    print "Total tweets",N
+    print "Positive ",float(p/N)*100,"%"
+    print "Negative ",float(n/N)*100,"%"
+    print "Neutral ",float(net/N)*100,"%"
--- sentimeter/views.py
+++ sentimeter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .forms import userinput
+import sentimeter
 
 
 def index(request):
@@ -13,6 +14,7 @@ def analyse(request):
     if request.GET and user_input.is_valid():
         input_hastag = user_input.cleaned_data['q']
         print input_hastag
+        sentimeter.primary(input_hastag)
         context={}
         return render(request, "index.html", {'input_hastag': user_input})
     return render(request, "index.html", {'input_hastag': user_input})
